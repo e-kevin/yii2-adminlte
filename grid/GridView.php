cf9d27211e83dd4259998bbb9ae76e71c209d8ac
E-Kevin <e-kevin@qq.com>
2017-05-16 22:37:44 +0800

    修正文档备注信息

--- grid/GridView.php
+++ grid/GridView.php
@@ -67,15 +67,17 @@ HTML;
     public $floatHeaderOptions = [];
 
     /**
-     * @var string panel|box组件的基础路由地址，一般在单页面同时显示多个panel|box组件时使用，
-     * 值为完整的路由地址，如：account/user/index、account/user/forbidden-list，默认值为`Yii::$app->controller->getRoute()`
+     * @var string panel|box组件的路由地址，一般在单页面同时显示多个box组件且多个（一个以上）组件的数据来源和当前的路由地址
+     * 不相同时使用，此种情况建议配置[[boxParams]]参数用于标识数据来源的组件用以返回相关数据。
+     * 该值为完整的路由地址，如：account/user/index、account/user/forbidden-list，默认值为`Yii::$app->controller->getRoute()`
      */
     public $boxUrl;
 
     /**
-     * @var array panel|box组件的url参数，一般在单页面同时显示多个panel|box组件时添加相应标识使用，
-     * 用于标识操作来源属于哪个panel|box组件。
-     * 如['from' => 'forbidden-list']，则可在[[Controller]]控制器里根据获取到的`from`参数值返回相应结果给客户端的panel|box组件
+     * @var array panel|box组件的url参数，一般在单页面同时显示多个box组件且多个（一个以上）组件的数据来源和当前的路由地址
+     * 不相同时使用，可以用于标识操作来源属于哪个组件。
+     * 如['from-box' => 'forbidden-list']，则可在相应的[[Controller]]控制器里根据获取到的`from-box`参数值返回相应结果
+     * 给客户端的组件
      */
     public $boxParams = [];
 
@@ -228,6 +230,22 @@ HTML;
         }
 
         return $this->_showToggle;
+    }
+
+    public function renderPager()
+    {
+        $renderPager = parent::renderPager();
+        $minCount = ArrayHelper::getValue($this->toggleDataOptions, 'minCount', 0);
+        $toggleData = Html::tag('span', '', [
+            'data-toggle-params' => Json::encode([
+                'useToggle' => $this->showToggle() ? 'true' : 'false', // 结合js动态显示切换按钮
+                'mode' => $this->_isShowAll ? 'all' : 'page',
+                'showMessage' => (!$minCount || $minCount >= $this->dataProvider->getTotalCount()) ? 'false' : 'true',
+                'message' => $this->toggleDataOptions['confirmMsg'],
+            ]),
+        ]);
+
+        return $this->showToggle() ? $renderPager . $toggleData : $renderPager;
     }
 
     protected function renderPanel()
@@ -491,22 +509,6 @@ HTML;
             'data-toggle' => 'tooltip',
             'title' => $defaultOptions['title'],
         ]);
-    }
-
-    public function renderPager()
-    {
-        $renderPager = parent::renderPager();
-        $minCount = ArrayHelper::getValue($this->toggleDataOptions, 'minCount', 0);
-        $toggleData = Html::tag('span', '', [
-            'data-toggle-params' => Json::encode([
-                'useToggle' => $this->showToggle() ? 'true' : 'false', // 结合js动态显示切换按钮
-                'mode' => $this->_isShowAll ? 'all' : 'page',
-                'showMessage' => (!$minCount || $minCount >= $this->dataProvider->getTotalCount()) ? 'false' : 'true',
-                'message' => $this->toggleDataOptions['confirmMsg'],
-            ]),
-        ]);
-
-        return $this->showToggle() ? $renderPager . $toggleData : $renderPager;
     }
 
     /**
